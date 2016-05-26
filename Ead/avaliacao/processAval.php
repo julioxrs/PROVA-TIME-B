2585976947ae6ba660d2f3a49f00842c7ddb9205
thiagoals <thiagowebdesigner@hotmail.com>
2016-05-26 00:57:49 -0300

    Update processAval.php
    Modificação de arquivo para adicionar a funcionalidade de inserir no banco de dados a avaliação feita do agendamento.

--- Ead/avaliacao/processAval.php
+++ Ead/avaliacao/processAval.php
@@ -1,13 +1,21 @@
 <?php
 	if((isset($_POST['notaAval']))&&(isset($_POST['idAgendamento']))&&(isset($_POST['Avaliar']))){
 		if((0<$_POST['notaAval'])&&($_POST['notaAval']<=5)){
-			//$_POST[''] = preg_replace('/[^[:num:]_]/', '',$_POST['notaAval']);
-			//$_POST[''] = preg_replace('/[^[:num:]_]/', '',$_POST['']);
-			//$_POST[''] = preg_replace('/[^[:num:]_]/', '',$_POST['login']);
-			//echo "not entre";
-			
+			include ("functions/agendamento.php");
+			include ("functions/connection.php");
+			$con = new Connection();
+			$pdo = $con->pdoConnect();
+			$agendamento = new Agendamento();
+			$agendamento->setid_agendamento($_POST['idAgendamento']);
+			$agendamento->setAval_Agendamento($_POST['notaAval']);
+			$resultAval_Agendamento = $agendamento->insertAvalValue($con, $pdo);
+			if($resultAval_Agendamento){
+				header("Location: index.php?msg=success");
+			}else{
+				header("Location: index.php?msg=wrong");
+			}
 		}else{
-			header("Location:index.php");
+			header("Location: index.php");
 		}
 	}
 ?>
